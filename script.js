5fa0aaa643419512982621c2d875ae5680b2e74e
JohnPaulInso <88543202+JohnPaulInso@users.noreply.github.com>
2024-07-08 18:41:51 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,42 @@
-	document.addEventListener('DOMContentLoaded', () => {
-	    const keys = document.querySelectorAll('.key');
-	    keys.forEach(key => {
-	        key.addEventListener('click', () => playSound(key.dataset.note));
-	    });
+document.addEventListener('DOMContentLoaded', () => {
+    const keys = document.querySelectorAll('.key');
+    const pressedKeys = {};
 
-	    document.addEventListener('keydown', (e) => {
-	        const key = e.key.toUpperCase();
-	        const pianoKey = document.querySelector(`.key[data-key="${key}"]`);
-	        if (pianoKey) {
-	            playSound(pianoKey.dataset.note);
-	            pianoKey.classList.add('pressed');
-	        }
-	    });
+    keys.forEach(key => {
+        key.addEventListener('click', () => playSound(key.dataset.note));
+    });
 
-	    document.addEventListener('keyup', (e) => {
-	        const key = e.key.toUpperCase();
-	        const pianoKey = document.querySelector(`.key[data-key="${key}"]`);
-	        if (pianoKey) {
-	            pianoKey.classList.remove('pressed');
-	        }
-	    });
-	});
+    document.addEventListener('keydown', (e) => {
+        const key = e.key.toUpperCase();
+        if (!pressedKeys[key]) {
+            const pianoKey = document.querySelector(`.key[data-key="${key}"]`);
+            if (pianoKey) {
+                playSound(pianoKey.dataset.note);
+                pianoKey.classList.add('pressed');
+                pressedKeys[key] = true;
+            }
+        }
+    });
 
-	function playSound(note) {
-	    const audio = new Audio(`sounds/${note}.wav`);
-	    audio.play();
+    document.addEventListener('keyup', (e) => {
+        const key = e.key.toUpperCase();
+        const pianoKey = document.querySelector(`.key[data-key="${key}"]`);
+        if (pianoKey) {
+            pianoKey.classList.remove('pressed');
+        }
+        pressedKeys[key] = false;
+    });
+});
 
-	    const pressedKey = document.querySelector(`.key[data-note="${note}"]`);
-	    if (pressedKey) {
-	        pressedKey.classList.add('pressed');
-	        setTimeout(() => {
-	            pressedKey.classList.remove('pressed');
-	        }, 200000);
-	    }
-	}
+function playSound(note) {
+    const audio = new Audio(`sounds/${note}.wav`);
+    audio.play();
+
+    const pressedKey = document.querySelector(`.key[data-note="${note}"]`);
+    if (pressedKey) {
+        pressedKey.classList.add('pressed');
+        setTimeout(() => {
+            pressedKey.classList.remove('pressed');
+        }, 200000);
+    }
+}
